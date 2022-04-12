e3f169655b6859bac7b34673d26c165c0fb963e2
earnestrichards <66366967+earnestrichards@users.noreply.github.com>
2022-04-12 16:45:27 -0500

    initial

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,26 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore soluta quas natus sit enim id alias earum consequatur ullam ut animi voluptatibus, dolorem suscipit eius. Illum deserunt dolor quibusdam velit."
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore soluta quas natus sit enim id alias earum consequatur ullam ut animi voluptatibus, dolorem suscipit eius. Illum deserunt dolor quibusdam velit."
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore soluta quas natus sit enim id alias earum consequatur ullam ut animi voluptatibus, dolorem suscipit eius. Illum deserunt dolor quibusdam velit."
+  },
+  {
+    id: 4,
+    rating: 7,
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore soluta quas natus sit enim id alias earum consequatur ullam ut animi voluptatibus, dolorem suscipit eius. Illum deserunt dolor quibusdam velit."
+  }
+
+]
+
+export default FeedbackData
+
